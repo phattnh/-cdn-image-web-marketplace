cd37b78c4eb27b2d451e4f9bc22258d152186b12
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-26 17:14:38 +0700

    Update dimession file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const sizeOf = require("image-size");
 require("dotenv").config();
 const fs = require("fs");
 const cors = require("cors");
@@ -17,33 +18,44 @@ app.get("/images", async (req, res) => {
   const fileName = `${src}-${width}px-${height}px-${fit}.${format}`;
   const imagesPath = path.resolve(__dirname, "public/images");
   const imageStaticPath = path.resolve(__dirname, `public/images/${fileName}`);
-  fs.access(imageStaticPath, (notExisted) => {
-    if (notExisted) {
-      fs.readFile(`${imagesPath}/${src}.webp`, (err, data) => {
-        if (err) {
-          return res
-            .status(400)
-            .json({ err, message: "Image src is not valid" });
-        }
-        sharp(data)
-          .resize({
-            width: Number(width),
-            height: Number(height),
-            fit: fit || "inside",
-          })
-          .toFormat(format, { quality: Number(quality) })
-          .toFile(imageStaticPath, (err, info) => {
-            if (info) {
-              res.sendFile(imageStaticPath);
-            } else {
-              res.status(400).json(err);
-            }
-          });
-      });
-    } else {
-      res.sendFile(imageStaticPath);
+  sizeOf(`${imagesPath}/${src}.webp`, (err, dimensions) => {
+    if (err) {
+      return res.status(400).json({ err, message: "Image src is not valid" });
     }
+    const { width: dw, height: dh, _format: dt } = dimensions;
+    let _width = Number(width);
+    let _height = Number(height);
+    let _quality = Number(quality);
+    const _format = format || dt;
+    const _fit = fit || "inside";
+    _width = isNaN(_width) ? dw : _width;
+    _height = isNaN(_height) ? dh : _height;
+    _quality = isNaN(_quality) ? 100 : _quality;
+    fs.readFile(`${imagesPath}/${src}.webp`, (err, data) => {
+      if (err) {
+        return res.status(400).json({ err, message: "Image src is not valid" });
+      }
+      const emitToImagePath = path.resolve(
+        __dirname,
+        `public/images/${src}-${_width}-${_height}-${_quality}-${_format}-${_fit}.${_format}`
+      );
+      sharp(data)
+        .resize({
+          width: _width,
+          height: _height,
+          fit: fit || "inside",
+        })
+        .toFormat(format, { quality: _quality })
+        .toFile(emitToImagePath, (err, info) => {
+          if (info) {
+            res.sendFile(emitToImagePath);
+          } else {
+            res.status(400).json(err);
+          }
+        });
+    });
   });
+  return;
 });
 
 server.listen(process.env.PORT, () => {
