cd1676ca78081e2d808fc5ec1ca03b99e85c1f96
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-26 16:14:22 +0700

    Resize image

---
+++ README.md
@@ -0,0 +1,33 @@
+## What is this project?
+
+- This project using for create order tracking service.
+- Socket IO - Realtime
+- Link: https://baemin-socket.herokuapp.com/
+- Copyright Thai Nguyen Hoang Phat
+
+## Available scripts
+
+In the project directory, you can run:
+
+| Command           |            Description             |       ENV        |
+| :---------------- | :--------------------------------: | :--------------: |
+| npm install       |            Install NPM.            |
+| npm start         |     Runs the app on port 3002      | .env.development |
+| npm run start:dev | Runs the app on dev mode port 3002 | .env.development |
+
+## How to run this project?
+
+- Copy .env.development -> .env
+- Run cmd npm start to start app on port 3002
+
+## Main Stack
+
+- Build socket io realtime app
+
+## Deploy
+
+- [Heroku](https://dashboard.heroku.com/apps/baemin-apis)
+
+## Production
+
+- https://phattnh.github.io/baemin-fe/
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require("dotenv").config();
+const fs = require("fs");
+const cors = require("cors");
+const http = require("http");
+const sharp = require("sharp");
+const server = http.createServer(app);
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hi! I'm CDN server"));
+
+app.get("/images", async (req, res) => {
+  const { src, width, height, quality, format } = req.query;
+  const fileName = `${src}-${width}px-${height}px.${format}`;
+  const imagesPath = path.resolve(__dirname, "public/images");
+  const imageStaticPath = path.resolve(__dirname, `public/images/${fileName}`);
+  fs.access(imageStaticPath, (notExisted) => {
+    if (notExisted) {
+      fs.readFile(`${imagesPath}/${src}.webp`, (err, data) => {
+        if (err) {
+          return res
+            .status(400)
+            .json({ err, message: "Image src is not valid" });
+        }
+        sharp(data)
+          .resize({
+            width: Number(width),
+            height: Number(height),
+          })
+          .toFormat(format, { quality: Number(quality) })
+          .toFile(imageStaticPath, (err, info) => {
+            if (info) {
+              res.sendFile(imageStaticPath);
+            } else {
+              res.status(400).json(err);
+            }
+          });
+      });
+    } else {
+      res.sendFile(imageStaticPath);
+    }
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Server listening on port", process.env.PORT);
+});
